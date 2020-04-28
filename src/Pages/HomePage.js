cbc47dd969b9d52135ca2c74fe12a2f8b3b57184
Elie Bismuth <eliebismuth91@gmail.com>
2020-04-28 23:50:18 +0200

    :white_check_mark: virgules nombres

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import {getApiEndpoint} from '../Components/api';
+import {numberWithCommas} from '../Utils/utils';
 
 const HomePage = () => {
 
@@ -9,9 +10,9 @@ const HomePage = () => {
 
 
     useEffect(() => {
-        getApiEndpoint('/deaths').then(response => setNbDeaths(response.data.latest))
-        getApiEndpoint('/confirmed').then(response => setNbConfirmed(response.data.latest))
-        getApiEndpoint('/recovered').then(response => setNbRecovered(response.data.latest))
+        getApiEndpoint('/deaths').then(response => setNbDeaths(numberWithCommas(response.data.latest)));
+        getApiEndpoint('/confirmed').then(response => setNbConfirmed(numberWithCommas(response.data.latest)));
+        getApiEndpoint('/recovered').then(response => setNbRecovered(numberWithCommas(response.data.latest)))
     }, [])
 
     return (
