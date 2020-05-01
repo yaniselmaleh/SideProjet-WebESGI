0beaccb2fbb26d863c56692ebe7443f6c79b33ac
yaniselmaleh <contact@yaniselmaleh.fr>
2020-05-01 18:57:33 +0200

    :art: Edit Attestation

--- src/Components/Attestation.js
+++ src/Components/Attestation.js
@@ -13,13 +13,20 @@ const GestesBarrieres = () => (
                 <h2 className="title-big">Attestation de déplacement dérogatoire</h2>
                 <p className="title-small">Un dispositif de création numérique de l’attestation de déplacement dérogatoire<br></br>est désormais disponible, en complément du dispositif papier toujours valide.</p>
                         
-                <div>
-                    <Cta className="unfilled">Version Papier</Cta>
-                    <Cta className="filled-blue">Version Numérique</Cta>
+                <div className="flex conteneur2">
+                    <Cta className="unfilled col-2">Version Papier</Cta>
+                    <Cta className="filled-blue col-2">Version Numérique</Cta>
                 </div>
             </div>
         </section>
-        <footer><p>Développé avec &#128525; par Elie & Yanis</p></footer>
+        <footer>
+            <p>
+                Développé avec &#128525; par  
+                <a href="https://www.linkedin.com/in/elie-bismuth" alt="Linkedin Elie Bismuth" target="_blank" rel="noopener noreferrer"> Elie </a>
+                & 
+                <a href="https://www.linkedin.com/in/yaniselmaleh" alt="Linkedin Yanis Elmaleh" target="_blank" rel="noopener noreferrer"> Yanis</a>
+            </p>
+        </footer>
     </>
 );
 
